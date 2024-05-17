ae7f1ea198e9cddce70335ec8379ae24b8a2b045
LiamKrenn <k.liam1224@gmail.com>
2024-05-17 17:10:25 +0200

    typo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,19 +2,10 @@
 	import { new_scramble, type } from '$lib/scramble';
 	import { onMount } from 'svelte';
 	import '../app.pcss';
-  import { pwaInfo } from 'virtual:pwa-info'; 
-
-  $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '' 
 
 </script>
 
-<svelte:head> 
- 	{@html webManifestLink} 
-</svelte:head>
-
 <title>scramblr</title>
-
-<meta name="theme-color" media="(prefers-color-scheme: dark)" content="#020817" />
 <slot />
 <h1
 	class="absolute left-0 top-0 ml-4 mt-2 flex cursor-pointer select-none flex-col text-3xl md:text-5xl"
@@ -23,7 +14,7 @@
 		class="font-semibold"
 		on:click={() => {
 			new_scramble($type);
-		}}>scramblrr</button
+		}}>scramblr</button
 	>
 
 	<a
@@ -36,14 +27,5 @@
 	</a>
 </h1>
 
-
-<style>
-  :global(*) {
-    --ion-safe-area-top: 0;
-    --ion-safe-area-bottom: 0;
-    --ion-safe-area-left: 0;
-    --ion-safe-area-right: 0;
-  }
-</style>
 
 
