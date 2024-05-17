07b3fa912884a5331a9eea1baf3131edd078955c
LiamKrenn <k.liam1224@gmail.com>
2024-05-17 21:11:16 +0200

    removed fullscreen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,6 @@
 	import { new_scramble, type } from '$lib/scramble';
 	import { onMount } from 'svelte';
 	import '../app.pcss';
-  import { Button } from '$lib/components/ui/button';
-  import { Maximize, Minimize } from 'lucide-svelte';
 
 	async function detectSWUpdate() {
 		const registration = await navigator.serviceWorker.ready;
@@ -22,33 +20,13 @@
 	}
 
 	onMount(async () => {
-    fullscreen = document.fullscreenElement !== null;
 		await detectSWUpdate();
 	});
-
-  let fullscreen = false;
-
-  function toggleFullscreen() {
-    if (fullscreen) {
-      document.exitFullscreen();
-    } else {
-      document.body.requestFullscreen();
-    }
-    fullscreen = !fullscreen;
-  }
 </script>
 
 <title>scramblr</title>
 <slot />
 
-<Button on:click={toggleFullscreen} variant="ghost" class="absolute bottom-0 right-0 p-2 m-2">
-  {#if fullscreen}
-    <Minimize />
-  {:else}
-    <Maximize />
-  {/if}
-
-</Button>
 <h1
 	class="absolute left-0 top-0 ml-4 mt-2 flex cursor-pointer select-none flex-col text-3xl md:text-5xl"
 >
