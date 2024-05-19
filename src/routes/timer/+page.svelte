5da916bcf78d0d7e4d705f27d87baf454c070f9c
LiamKrenn <k.liam1224@gmail.com>
2024-05-19 23:15:03 +0200

    layout improv

--- src/routes/timer/+page.svelte
+++ src/routes/timer/+page.svelte
@@ -24,9 +24,9 @@
 	<ScrambleSelector />
 	<div
 		id="scrambleContainer"
-		class="absolute md:top-4 top-20 block h-[32%] max-h-[32%] md:mx-36 select-text text-balance text-center text-3xl !leading-[125%]"
+		class="absolute md:top-4 top-20 block h-[32%] max-h-[32%] md:mx-36 text-balance text-center text-3xl !leading-[125%]"
 	>
-		<p class="-z-10 w-full cursor-default select-none px-4" id="scramble">
+		<p class="-z-10 w-full cursor-default  select-none px-4" id="scramble">
 			{$scramble}
 		</p>
 	</div>
@@ -42,7 +42,7 @@
     </div>
     <!-- Middle -->
 
-    <div class="grow xl:flex flex-col hidden bg-gray-500">
+    <div class="grow lg:flex flex-col hidden bg-gray-500">
       <!-- Stats -->
       <div class="bg-orange-500 grow w-full">
         stats
@@ -63,18 +63,18 @@
     </div>
 
     <!-- Right -->
-    <div class="flex flex-col xl:grow grow-[0.3] bg-violet-500">
+    <div class="flex flex-col lg:grow grow-[0.3] bg-violet-500">
       
       <!-- Preview -->
-      <ScramblePreview class="bg-green-500 grow-0 w-full xl:h-full h-[30%]"/>
+      <ScramblePreview class="bg-green-500 grow-0 w-full lg:h-full h-[30%]"/>
       
       <!-- Stats -->
-      <div class="xl:hidden flex bg-orange-500 grow w-full">
+      <div class="lg:hidden flex bg-orange-500 grow w-full">
         stats
       </div>
 
       <!-- Logo -->
-      <h1 class="xl:hidden flex cursor-pointer select-none flex-col text-3xl lg:text-4xl xl:text-5xl bg-black">
+      <h1 class="lg:hidden flex cursor-pointer select-none flex-col text-3xl lg:text-4xl lg:text-5xl bg-black">
         <p class=" font-semibold opacity-90">scramblr</p>
       
         <a
