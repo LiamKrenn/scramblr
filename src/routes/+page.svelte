3a5a248a8fa4ffc7d411d2d298a6195a97774f5b
LiamKrenn <k.liam1224@gmail.com>
2024-05-20 19:28:52 +0200

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,21 +29,22 @@
 
 <svelte:window on:keydown|preventDefault={onKeyDown} />
 
-<h1 class="absolute left-4 md:bottom-4 md:top-auto top-4 flex cursor-pointer select-none flex-col text-3xl lg:text-4xl xl:text-5xl">
-  <p class="-z-10 font-semibold opacity-90">scramblr</p>
+<h1
+	class="absolute left-4 top-4 flex cursor-pointer select-none flex-col text-3xl md:bottom-4 md:top-auto lg:text-4xl xl:text-5xl"
+>
+	<p class="-z-10 font-semibold opacity-90">scramblr</p>
 
-  <a
-    class="!z-20 text-sm opacity-75 lg:mt-1 md:text-base"
-    href="https://github.com/LiamKrenn"
-    target="_blank"
-  >
-    by
-    <span class="underline"> Liam Krenn </span>
-  </a>
+	<a
+		class="!z-20 text-sm opacity-75 md:text-base lg:mt-1"
+		href="https://github.com/LiamKrenn"
+		target="_blank"
+	>
+		by
+		<span class="underline"> Liam Krenn </span>
+	</a>
 </h1>
 
-
-<Menu class="!z-30"/>
+<Menu class="!z-30" />
 <div class="relative z-10 flex h-full w-full items-center justify-center">
 	<ScrambleSelector />
 	<ScramblePreview class="absolute bottom-4 right-0 max-h-[20%] max-w-full px-2" />
@@ -51,13 +52,28 @@
 		on:click={click}
 		class="absolute flex h-full w-full cursor-pointer flex-col items-center justify-center"
 	/>
+  
 	<button
 		id="scrambleContainer"
 		on:click={textClick}
-		class="absolute mx-[8%] h-[40%] max-h-[40%] select-text text-balance text-center text-5xl !leading-[125%]"
+		class="absolute mx-[8%] h-[40%] max-h-[40%] select-text text-balance block text-center"
 	>
-		<p class="w-full cursor-text px-4" id="scramble">
-			{$scramble}
-		</p>
+		{#if $scramble.length > 200}
+			<p class="scramble text-balance text-[3.3cqmin] ">
+				{$scramble}
+			</p>
+		{:else if $scramble.length > 100}
+			<p class="scramble text-balance text-[5cqmin]">
+				{$scramble}
+			</p>
+		{:else if $scramble.length > 50}
+			<p class="scramble text-balance text-[6cqmin]">
+				{$scramble}
+			</p>
+		{:else}
+			<p class="scramble text-balance text-[7cqmin]">
+				{$scramble}
+			</p>
+		{/if}
 	</button>
 </div>
