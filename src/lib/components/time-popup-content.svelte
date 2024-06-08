f50a9e57ef66166adf5d89115bcfbf06916a0b55
LiamKrenn <k.liam1224@gmail.com>
2024-06-08 22:15:46 +0200

    prisma vercel fix

--- src/lib/components/time-popup-content.svelte
+++ src/lib/components/time-popup-content.svelte
@@ -18,7 +18,7 @@
   <Button on:click={() => {deleteTime(time.id)}} variant="destructive" class="absolute mb-1 top-4 right-4">Delete <Trash class="p-1 -mr-1"/> </Button>
   <p class="mb-1">Scramble: <span class="bg-slate-800 px-1 rounded">{time.scramble}</span></p>
   <p class="mb-1">Comment: </p>
-  <p class="mb-1">Solved on: {new Date(time.timestamp).toISOString().replace("T"," ").slice(0,19)}</p>
+  <p class="mb-1">Solved on: {new Date(time.timestamp).toLocaleString()}</p>
   <div class="flex">
     <Button on:click={close} variant="outline" class="py-1 h-10 px-0 grow-[0.2] mr-2 mt-4">Cancel</Button>
     <Button on:click={close} variant="secondary" class="py-1 h-10 px-0 grow  mt-4">Save</Button>
