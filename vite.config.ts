12d32dd571ccc875ea82fbfab52570b219a0ce09
Liam Krenn <k.liam1224@gmail.com>
2025-03-29 20:38:07 +0100

    fix vite build

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,24 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit()],
+  worker: {
+    format: "es", // Ensure workers are built as ES modules,
+    rollupOptions: {
+      output: {
+        generatedCode: "es2015",
+      },
+    },
+  },
   // server: { fs: { allow: ["./triplit"] } },
   optimizeDeps: {
     exclude: ["@electric-sql/pglite"],
+  },
+  build: {
+    target: "es2022", // Ensure modern JS features are supported
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true, // Enable dynamic imports for better chunking
+      },
+    },
   },
 });
